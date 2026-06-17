6b9ea01cf70970baa18a903a715afb61dfff135c
Cueva <qeva.ai.solutions@gmail.com>
2026-06-17 20:12:58 -0300

    fix: ScrollToTop al navegar entre paginas

--- src/landing/main.tsx
+++ src/landing/main.tsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes, useLocation } from 'react-router-dom'
 import Landing from '../pages/Landing'
 import OctopusFlow from '../pages/OctopusFlow'
 import ProductAccess from '../pages/ProductAccess'
 import '../styles/globals.css'
 
+function ScrollToTop() {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+
+  return null
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Landing loginUrl="/acceder" />} />
         <Route path="/octopusflow" element={<OctopusFlow />} />
